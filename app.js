a0a21eb42fee0f16fa1f931833d540212920ebb2
Ashley Davis <ashley@codecapers.com.au>
2016-11-09 12:03:47 +1000

    Setup permanent sidebar with search filter.

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
 
 angular.module( 'YourApp', [ 'ngMaterial', 'ngMdIcons' ] )
-    .controller("YourController", function ($scope, $mdSidenav) {
-        $scope.yo = 'wddw!';
-
-        $scope.closeLeftSideNav = function () {
-            $mdSidenav('left').close();
-        };
-
-        $scope.toggleLeftSideNav = function () {
-            $mdSidenav('left').toggle();
-        };
+    .controller("YourController", function ($scope) {
 
     });
 
